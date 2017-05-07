52f285183e618ccdd57420377b27090f968cb41f
micahjmartin <mjm5097@rit.edu>
2017-05-06 20:48:31 -0400

    Added Color

--- scrape.sh
+++ scrape.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
 # Search github, scrape for removed keys in commits
+
+# colors for display
+NOCOLOR='\033[0m'
+red() { CRED='\033[0;31m'; echo -e ${CRED}$@${NOCOLOR}; }
+blue() { CBLUE='\033[0;34m'; echo -e ${CBLUE}$@${NOCOLOR}; }
+green() { CGREEN='\033[0;32m'; echo -e ${CGREEN}$@${NOCOLOR}; }
+
+up() {
+    printf "\033[$1A"
+    for i in `seq $1`; do
+	echo -e "\033[K"
+    done
+    printf "\033[$1A"
+}
+
 search_term="removed+private+key"
 
 get_commits() {
     search=$(curl -s "https://github.com/search?q=$search_term&type=Commits")
     links=$(echo "$search" | grep "Browse the repository at this point in the history")
     repos=$(echo "$links" | sed 's/.*href="\(.*\)" aria.*/\1/p' | uniq)
-    echo "[+] Found" $(echo "$repos" | wc -l) "Projects"
+    echo "[+] Found" $(echo "$repos" | wc -l) "project(s)"
 }
 
 get_files() {
@@ -17,42 +32,42 @@ get_files() {
 	cd $dirr
 	rm -fr tmp/curr	
 	cd tmp
-	echo "[*] Cloning $name..."
+	blue "[*] Cloning $name..."
 	git clone https://github.com/$name curr &>/dev/null
 	# If this fail then skip all the next stuff
 	if [ $? = 0 ]; then
-	#echo "[*] Working on $name"	
 	cd curr
 
 	# Make sure the commit is still there
 	git cat-file $commit~1 -t &>/dev/null
 	retval=$?
 	if [ $retval != 0 ]; then
-	    echo "[!] Error commit doesnt exist $commit~1"
+	    up 1
+	    red "[$name] Error commit doesnt exist $commit~1"
 	    cd ../../
 	    rm -fr tmp/curr
 	    continue
 	else
-	    :
-	    #echo "[+] Checked out $commit~1"
+	    up 1
+	    green "[$name] Commit exists $commit~1"
 	fi
 	
 	# Get the files that have changed
 	files=$(git diff "$commit~1" $commit --name-only )
-	echo "$files"
-	echo "[+]" $(echo "$files" | wc -l)" file(s) found"
+	num_fils=$(echo "$files" | wc -l)
+	
         # Checkout the commit before the change
 	git checkout $commit~1 &>/dev/null
-	retval=$?
-	if [ $retval != 0 ]; then
-	    echo "[!] Error cannot checkout $commit~1"
+	if [ $? != 0 ]; then
+	    up 1
+	    red "[$name] Error cannot checkout $commit~1"
 	    continue
 	else
-	    :
-	    #echo "[+] Checked out $commit~1"
+	    up 1
+	    green "[$name] Checked out $commit~1"
 	fi
 	
-	#grep -l 'PRIVATE KEY' -r ./
+	good_files=0
 	for f in $files; do
 	    # Make sure the file is ASCII
 	    isascii="$(file $f | grep -e '(ASCII|TEXT'))"
@@ -61,25 +76,31 @@ get_files() {
 		change=$(git show "$commit~1":$f)
 		# Check if this contains a key
 		if [ "$(echo $change | grep 'PRIVATE KEY')" != "" ]; then
-		    echo -e "\t[+] $f has potential"
-		    # Save the file that has potential
-		    output_folder="$dirr/found/$(echo $name| cut -d'/' -f2)"
-		    mkdir -p $output_folder
+		    good_files=$(($good_files +1))
+		    # Make the output fold and make the filename
+		    mkdir -p $dirr/found
 		    fil=$(echo $f | sed 's=.*/==' )
-		    echo "$f - $fil"
-		    git show "$commit~1":$f >> "$output_folder/$fil"
+		    output_folder="$dirr/found/$(echo $name| cut -d'/' -f2)-$fil"
+		    # Save the file that has potential
+		    git show "$commit~1":$f >> "$output_folder"
 		fi
 	    else
-		echo "[!] Non-text $f $(file $f)"
+		:
+		#echo "[!] Non-text $f $(file $f)"
 	    fi
 	done
-	#echo $files
-	#| sed -e '/BEGIN/,/END PRIVATE/!d')
-	#key=$(echo $diff | sed 's:\n:\\n:p' |\
-	#grep -o --color "BEGIN PRIVATE KEY.*END PRIVATE KEY")
-	#echo $diff
+
+	# Print the status
+	up 1
+	if [ $good_files != 0 ]; then
+	    green "[$name] $good_files/$num_fils files have potential"
 	else
-	    echo "[!] Error cloning $name"
+	    echo "[$name] $good_files/$num_fils files have potential"
+	fi
+
+	else
+	    up 1
+	    red "[!] Error cloning $name"
 	fi
 	#echo [+] Done with $name
     done
@@ -96,3 +117,5 @@ dirr=$PWD
 init
 get_commits
 get_files
+
+echo "[*] Found $(ls $dirr/found | wc -w) files"
